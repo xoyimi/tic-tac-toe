af4a0205516e46759dc83736ef0537087b2eefed
xoyimi_jingdao <xoyimi@foxmail.com>
2020-07-29 17:14:19 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <template v-for="row in map">
+            <template v-for="(row,index) in row">
+                <Cell @cellClick="cellClick(index)"/>
+            </template>
+        </template>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Cell from "./components/Cell";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Cell
+        },
+        data() {
+            return {
+                count: 0,
+                map: [
+                    [null, null, null],
+                    [null, null, null],
+                    [null, null, null]
+                ]
+            }
+        },
+        methods: {
+            cellClick(index) {
+                console.log(index)
+            }
+        }
+        ,
+        created() {
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    :root {
+        touch-action: none;
+        height: 100%;
+        font-size: calc(750 / 100 * 100vw);
+    }
+
+    body {
+        margin: 0;
+        height: 100%;
+        font-size: calc(12px + .5vw);
+    }
+
+    #app {
+
+    }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cell" @click="cellClick">{{status}}</div>
+</template>
+
+<script>
+    export default {
+        name: "Cell",
+        data(){
+            return{
+                status:''
+            }
+        },
+        methods:{
+            cellClick(){
+                this.status='X'
+                this.$emit('cellClick')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.cell{
+    max-width: 200px;
+    max-height: 200px;
+    height: 25vw;
+    width: 25vw;
+    border: 1px solid pink;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: .02rem;
+}
+</style>
